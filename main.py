11c7e430036906ff76f0f7f8a4f0944527f834cf
DenisTsh01 <98765891+DenisTsh01@users.noreply.github.com>
2023-04-07 18:13:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def sync(main_folder, backup_folder, log_message):
     datenow = datetime.now()
 
-###############################################################If files don't exist
+###If files don't exist
     if os.path.exists(main_folder) == 0:  # if main folder doesn't exist, we create one
         os.makedirs(main_folder)
         print("Backup folder created successfully")
@@ -33,7 +33,7 @@ def sync(main_folder, backup_folder, log_message):
     with open(main_log_message, 'a') as log_file:
         log_file.write(f"Log: {datenow}: Syncing ...\n")
     print(f"Log: {datenow}: Syncing ...")
-############################################################################################################
+###
 
     all_from = os.listdir(main_folder)  # list of all files from our main folder
     all_to = os.listdir(backup_folder)  # list of existing files of backup folder
@@ -60,18 +60,18 @@ def sync(main_folder, backup_folder, log_message):
                     f"{datenow}: File {file} was successfully removed from {os.path.basename(backup_folder)}. \n")
             print(message2)
 
-    for root, dirs, files in os.walk(backup_folder):
+    for root, dirs, files in os.walk(backup_folder): #updating files content
         for file in all_from:
             backup_file_path = os.path.join(root, file)
             main_file_path = os.path.join(main_folder, file)
 
-            with open(backup_file_path, 'r') as backup_file:
+            with open(backup_file_path, 'r') as backup_file: # store the files
                 backup_content = backup_file.read()
 
-            with open(main_file_path, 'r') as main_file:
+            with open(main_file_path, 'r') as main_file: # store the files
                 main_content = main_file.read()
 
-            if backup_content != main_content:
+            if backup_content != main_content: # comparing the content
                 main_log_message = os.path.join(main_folder, log_message)
 
                 backup_log_message = os.path.join(root, os.path.basename(log_message))
